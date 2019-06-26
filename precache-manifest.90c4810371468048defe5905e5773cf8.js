aeab6ef27f08dc846ed1a9aca65be4b68963510b
Tran Thanh huy <tranthanhhuy@MacBook-Air-cua-Tran.local>
2019-06-26 13:27:21 +0700

    Updates

---
+++ precache-manifest.90c4810371468048defe5905e5773cf8.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e8ad23a10c07ca4b16118f10dbd1f4",
+    "url": "/hdexpertise/index.html"
+  },
+  {
+    "revision": "b80846a546b529ffbdba",
+    "url": "/hdexpertise/static/css/main.319295a1.chunk.css"
+  },
+  {
+    "revision": "639639f055c226084fcf",
+    "url": "/hdexpertise/static/js/2.eaba30a0.chunk.js"
+  },
+  {
+    "revision": "b80846a546b529ffbdba",
+    "url": "/hdexpertise/static/js/main.e18ed212.chunk.js"
+  },
+  {
+    "revision": "f903bbe4b99a2d151999",
+    "url": "/hdexpertise/static/js/runtime~main.13f3fa72.js"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/hdexpertise/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "f1b8b8cf3d66e8e2453842a6779a848c",
+    "url": "/hdexpertise/static/media/bestsystems.f1b8b8cf.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hdexpertise/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hdexpertise/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hdexpertise/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hdexpertise/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hdexpertise/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/hdexpertise/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "eda2636519b5451bc99704a6125f4a21",
+    "url": "/hdexpertise/static/media/good.eda26365.jpg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/hdexpertise/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/hdexpertise/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/hdexpertise/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "ba449a0697388668ef05e043ffdeafca",
+    "url": "/hdexpertise/static/media/img-2-1200x1000.ba449a06.jpg"
+  },
+  {
+    "revision": "0ec264785c9b6efbed2651d84105f436",
+    "url": "/hdexpertise/static/media/landing.0ec26478.jpg"
+  },
+  {
+    "revision": "69a14059626cb3ac3d9d0e974d45ad31",
+    "url": "/hdexpertise/static/media/logochu.69a14059.png"
+  },
+  {
+    "revision": "aa283fbe9bda3d77fc72c05616aeb6e1",
+    "url": "/hdexpertise/static/media/myobLogo.aa283fbe.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/hdexpertise/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/hdexpertise/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/hdexpertise/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/hdexpertise/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/hdexpertise/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "278a0d250cc46de3a4f65a0791479e71",
+    "url": "/hdexpertise/static/media/profile.278a0d25.jpg"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/hdexpertise/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/hdexpertise/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/hdexpertise/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/hdexpertise/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/hdexpertise/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HDexpertise/precache-manifest.3fe89243381e40cb8f967ef4c7ea26a3.js"
+  "/hdexpertise/precache-manifest.90c4810371468048defe5905e5773cf8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HDexpertise/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hdexpertise/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
